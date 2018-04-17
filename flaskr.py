c65f468a38cb04d48551ef58425252aac63f3caa
shepherd_dc <shepherd_dc@aliyun.com>
2018-04-17 16:57:53 +0800

    config.py

--- flaskr.py
+++ flaskr.py
@@ -1,9 +1,11 @@
 import os
 import sqlite3
+import config
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+# app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_object(config)
 
 @app.route('/')
 def index():
@@ -15,6 +17,12 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 if __name__ == '__main__':
-    app.debug = True
     app.run()
